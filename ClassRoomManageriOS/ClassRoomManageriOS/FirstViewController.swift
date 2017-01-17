78e1378017bfaf7108c8a480bf04d3ee8e3249ed
kmorrical <kathryn.morrical@gmail.com>
2017-01-17 12:53:23 -0800

    first commit

---
+++ ClassRoomManageriOS/ClassRoomManageriOS/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  ClassRoomManageriOS
+//
+//  Created by Kathryn Morrical on 1/17/17.
+//  Copyright © 2017 Kathryn Morrical Coding. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ClassRoomManageriOS/ClassRoomManageriOS/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  ClassRoomManageriOS
+//
+//  Created by Kathryn Morrical on 1/17/17.
+//  Copyright © 2017 Kathryn Morrical Coding. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
